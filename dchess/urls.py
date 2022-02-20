0d404ef716d917faa582d1adeb80ba13015ead99
Jan Rokicki <rokicki.jan@gmail.com>
2022-02-20 16:46:22 +0100

    Root url

--- dchess/urls.py
+++ dchess/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from game import views as game_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('game/', include('game.urls'))
+    path('game/', include('game.urls')),
+    path('', game_views.index, name='home')
 ]
